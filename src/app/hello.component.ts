ca6eb8e81967655696dffc9982a8e9ca2a450a96
truongphuquocc <tpquoc22@gmail.com>
2023-07-12 08:39:18 +0700

    Structural Directive

---
+++ src/app/hello.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Hello {{ name }}</h1>`,
+  styles: [
+    `
+      h1 {
+        font-family: lato;
+      }
+    `,
+  ],
+})
+export class HelloComponent {
+  @Input()
+  name!: string;
+
+  ngOnInit() {
+    console.log('hello init');
+  }
+  ngOnDestroy() {
+    console.log('destroy');
+  }
+}
